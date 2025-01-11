e575156a743ee3c3be61aabb53338b5fbc8ca8cc
Sam <armandawoodkhan@gmail.com>
2025-01-11 22:07:36 +0530

    Add getUserTweets controller

--- YoutubeAPI/src/controllers/tweet.controller.js
+++ YoutubeAPI/src/controllers/tweet.controller.js
@@ -30,10 +30,55 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const { userId } = req.params;
+    if (!userId) {
+        throw new ApiError(400, "UserId is missing")
+    }
+
+    const tweets = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "tweets",
+                localField: "_id",
+                foreignField: "owner",
+                as: "tweets"
+            }
+        },{
+            $addFields: {
+                tweets: {
+                    $first: "$tweets"
+                }
+            }
+        },{
+            $project:{
+                fullName: 1,
+                tweets: 1,
+                userName: 1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+    ])
+    if (!tweets?.length) {
+        throw new ApiError(404,"Tweets does not exist")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, tweets,"Tweets fetched Successfully")
+    )
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
